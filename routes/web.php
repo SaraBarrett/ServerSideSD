15ac991d346a1efb6ee17328e529ae3c41941fa4
SaraBarrett <sara.c.mon@gmail.com>
2025-01-20 15:29:12 +0000

    routes

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,19 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/home', function(){
+    return "<a href=". route('users.show')." >Clica aqui para veres todos os users</a>";
+})->name('home');
+
+Route::get('/users', function(){
+    return '<h1>Aqui vês todos os users</h1>';
+})->name('users.show');
+
+Route::get('/hello/{name}', function($name){
+    return '<h1>Hello</h1>'.$name;
+});
+
+Route::fallback(function(){
+    return "<a href=". route('home')." >estás perdido volta aqui</a>";
+});
