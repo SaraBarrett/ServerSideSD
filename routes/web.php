1da30482e12825fca067518a377ecd6a2fcd9592
SaraBarrett <sara.c.mon@gmail.com>
2025-01-20 16:20:44 +0000

    notes

--- routes/web.php
+++ routes/web.php
@@ -6,18 +6,33 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+// rota home
 Route::get('/home', function(){
     return view('home');
 })->name('home');
 
+
+// rotas users
+//rota para no futuro carregar uma tabela com todos os users
 Route::get('/users', function(){
     return view('users.all_users');
 })->name('users.show');
 
+
+//rota com paramentros
 Route::get('/hello/{name}', function($name){
     return '<h1>Hello</h1>'.$name;
 });
 
+//rota fallback: cai aqui quando não encontra nenhuma rota com o url solicitado no frontend
 Route::fallback(function(){
     return "<a href=". route('home')." >estás perdido volta aqui</a>";
 });
+
+
+
+
+
+
+//Notas:
+//os nomes das rotas servem para se identificar as mesmas dentro do código com uma "key", como por exemplo para chamar no href
