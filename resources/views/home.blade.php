231e1c1ac9b4730f8ceb71da65ec98dd11b9e133
SaraBarrett <sara.c.mon@gmail.com>
2025-01-21 09:40:33 +0000

    layout master

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,22 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-    <link rel="stylesheet" href="{{asset('css/style.css')}}">
-</head>
-
-<body>
+@extends('layouts.fo_layout')
+@section('content')
     <h5>Olá estou em casa.</h5>
     <img src="{{asset('images/what-is-software-CA-Capterra-Header.png')}}" alt="">
     <ul>
         <li><a href="{{ route('users.show') }}">Todos os users</a></li>
         <li><a href="{{ route('users.add') }}">Adicionar Utilizador</a></li>
     </ul>
-
-</body>
-
-</html>
+@endsection
